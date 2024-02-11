23a59c310b0931157b9c7572dc6fb3e80d61478e
justfishin1999 <justfishin@icloud.com>
2024-02-10 20:26:12 -0600

    Add sendable chooser for selecting auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,14 @@
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -44,6 +48,8 @@ public class RobotContainer {
     private final Shooter s_Shooter = new Shooter();
     private final Indexer s_Indexer = new Indexer();
 
+    private final SendableChooser<Command> autoChooser;
+
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -61,10 +67,18 @@ public class RobotContainer {
     NamedCommands.registerCommand("RunIndex", RunIndex());
     NamedCommands.registerCommand("StopIndex", StopIndex());
 
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Mode", autoChooser);
+
+    configureBindings();
         // Configure the button bindings
         configureButtonBindings();
     }
 
+
+    private void configureBindings(){
+        SmartDashboard.putData("Auto Chooser",new PathPlannerAuto("Shoot-Pick-Shoot-Auto"));
+    }
     /**
      * Use this method to define your button->command mappings. Buttons can be created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
