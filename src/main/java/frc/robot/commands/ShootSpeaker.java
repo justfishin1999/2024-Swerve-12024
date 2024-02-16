fed4b81c4e54d3c5b2f5c550031006227e35db33
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2024-02-16 11:32:59 -0600

    Update ShootSpeaker.java

--- src/main/java/frc/robot/commands/ShootSpeaker.java
+++ src/main/java/frc/robot/commands/ShootSpeaker.java
@@ -4,14 +4,17 @@ import frc.robot.subsystems.Shooter;
 
 public class ShootSpeaker extends Command{
     private Shooter s_Shooter;
+    private int c_velocity;
 
-    public ShootSpeaker(Shooter s_Shooter){
+    public ShootSpeaker(Shooter s_Shooter, int c_velocity){
         this.s_Shooter = s_Shooter;
+        this.c_velocity = c_velocity;
         addRequirements(s_Shooter);
     }
+    
     public void execute(){
         //Set the shooter to the desired speed
-        s_Shooter.shootSpeaker(4000);
+        s_Shooter.shootSpeaker(c_velocity);
     }
     public void isInterrupted(){
         //Stop shooter if command is interrupted
